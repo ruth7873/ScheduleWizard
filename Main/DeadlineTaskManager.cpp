6d314838c419da78912629911e25d859dbaa063c
ruth7873 <ruth0533137873@gmail.com>
2024-09-29 15:29:23 +0300

    deadline correct

--- Main/DeadlineTaskManager.cpp
+++ Main/DeadlineTaskManager.cpp
@@ -5,46 +5,50 @@
 std::priority_queue<std::shared_ptr<DeadlineTask>, std::vector<std::shared_ptr<DeadlineTask>>, DeadlineTaskManager::CompareDeadline> DeadlineTaskManager::minHeap;
 
 void DeadlineTaskManager::addTask(const std::shared_ptr<DeadlineTask>& task) {
-    if (task->getPriority() != PrioritiesLevel::CRITICAL)
-        minHeap.push(task);
+	if (task->getPriority() != PrioritiesLevel::CRITICAL)
+		minHeap.push(task);
 }
 
 std::shared_ptr<DeadlineTask> DeadlineTaskManager::getUpcomingTask() {
-    if (minHeap.empty()) {
-        cerr << "Heap is empty, can't pop\n";
-        return nullptr;
-    }
-    return minHeap.top();
+	if (minHeap.empty()) {
+		cerr << "Heap is empty, can't pop\n";
+		return nullptr;
+	}
+	return minHeap.top();
 }
 
 
 void DeadlineTaskManager::deadlineMechanism() {
-    if (!minHeap.empty()) {
-        std::shared_ptr<DeadlineTask> earliestTask = minHeap.top();
-        time_t currentTime = time(nullptr) * 100;
-        // Check if the current time is close to the task's deadline
-        if (currentTime >= earliestTask->getDeadline() - earliestTask->getRunningTime() &&
-            earliestTask->getPriority() != PrioritiesLevel::CRITICAL &&
-            earliestTask->getStatus() != TaskStatus::COMPLETED &&
-            earliestTask->getStatus() != TaskStatus::TERMINATED) {
+	while (true) {
+		if (!minHeap.empty()) {
+			std::shared_ptr<DeadlineTask> earliestTask = minHeap.top();
+			time_t currentTime = time(nullptr) * 100;
+			// Check if the current time is close to the task's deadline
+			if (currentTime >= earliestTask->getDeadline() - earliestTask->getRunningTime() &&
+				earliestTask->getPriority() != PrioritiesLevel::CRITICAL &&
+				earliestTask->getStatus() != TaskStatus::COMPLETED &&
+				earliestTask->getStatus() != TaskStatus::TERMINATED) {
 
-            // Change the status of the task
-            earliestTask->setPriority(PrioritiesLevel::CRITICAL);
+				// Change the status of the task
+				earliestTask->setPriority(PrioritiesLevel::CRITICAL);
 
-            // Insert the task into the scheduler
-            Scheduler::insertTask(dynamic_pointer_cast<Task>(earliestTask));
-            // Remove the task from the heap
-            {
-                std::unique_lock<std::mutex> lock(Scheduler::rtLock);  // Lock the rtLock
-                if (!minHeap.empty())
-                    minHeap.pop();
-            }
-        }
-        else {
-            if (earliestTask->getPriority() == PrioritiesLevel::CRITICAL ||
-                earliestTask->getStatus() == TaskStatus::COMPLETED)
+				// Insert the task into the scheduler
+				Scheduler::insertTask(dynamic_pointer_cast<Task>(earliestTask));
+				// Remove the task from the heap
+				{
+					std::unique_lock<std::mutex> lock(Scheduler::rtLock);  // Lock the rtLock
+					if (!minHeap.empty())
+						minHeap.pop();
+				}
+			}
+			else {
+				if (earliestTask->getPriority() == PrioritiesLevel::CRITICAL ||
+					earliestTask->getStatus() == TaskStatus::COMPLETED)
 
-                minHeap.pop();
-        }
-    }
+					minHeap.pop();
+			}
+		}
+		std::this_thread::sleep_for(std::chrono::milliseconds(1)); // Check every second
+
+	}
 }
